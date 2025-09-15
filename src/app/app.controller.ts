109678dd1d79494182cd6acb9bde92d298016173
Guilherme Oliveira Casagrande <32659090+ryguigas0@users.noreply.github.com>
2025-09-15 07:43:35 -0300

    made version route public

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from 'src/auth/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Public()
   getVersion(): string {
     return this.appService.getVersion();
   }
