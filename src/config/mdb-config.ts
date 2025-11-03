d05038c1acc911b0af902b6951c1a3b6a77612a8
Guilherme Oliveira Casagrande <32659090+ryguigas0@users.noreply.github.com>
2025-11-03 20:23:38 -0300

    changed to supply mongodb url

--- src/config/mdb-config.ts
+++ src/config/mdb-config.ts
@@ -4,14 +4,8 @@ import { MongooseModuleOptions } from '@nestjs/mongoose';
 export const getMongooseOptions = (
   configService: ConfigService,
 ): MongooseModuleOptions => {
-  const user = configService.getOrThrow<string>('MONGO_USER');
-  const pass = configService.getOrThrow<string>('MONGO_PASS');
-  const host = configService.getOrThrow<string>('MONGO_HOST');
-  const port = configService.getOrThrow<number>('MONGO_PORT');
-  const db = configService.getOrThrow<string>('MONGO_DB');
+  const uri = configService.getOrThrow<string>('MONGO_URI');
   const authSource = configService.getOrThrow<string>('MONGO_AUTH_SOURCE');
-
-  const uri = `mongodb://${user}:${pass}@${host}:${port}/${db}`;
 
   return {
     uri,
